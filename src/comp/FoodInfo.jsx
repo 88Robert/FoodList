0f592ca4cb27b6463055b8c2baaa3caeb61ee131
88Robert <robert.magnusson1988@gmail.com>
2024-03-07 11:39:24 +0100

    detail info OK

--- src/comp/FoodInfo.jsx
+++ src/comp/FoodInfo.jsx
@@ -1,20 +1,34 @@
-import { Container, Row, Col } from "react-bootstrap"
-
+import { Container, Row, Col } from "react-bootstrap";
 
 const FoodInfo = ({ food }) => {
+  const filterIngredients = (food) => {
+    const ingredients = Array.from({ length: 20 }, (_, i) => {
+      const ingredient = food[`strIngredient${i + 1}`];
+      const measure = food[`strMeasure${i + 1}`];
+      return ingredient && measure && `${ingredient} - ${measure}`;
+    }).filter(Boolean);
+    return ingredients;
+  };
   return (
     <Container>
       <Row>
         <div>
-          <h2>Dina Maträtter</h2>
-          <p>Title: {food.strMeal}</p>
-          <img src={food.strMealThumb} alt={food.strMeal} />
-          <p>Category: {food.strCategory}</p>
-          <p>Area: {food.strArea}</p>
+          <h3>Dina Maträtter</h3>
+          <p>
+            <strong>Title:</strong> {food.strMeal}
+          </p>
+          <img className="Foodpic" src={food.strMealThumb} alt={food.strMeal} />
+          <h5>Ingredients:</h5>
+          <ul>
+            {filterIngredients(food).map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+          <h5>Info:</h5> <p>{food.strInstructions}</p>
         </div>
       </Row>
     </Container>
   );
 };
 
-export default FoodInfo
+export default FoodInfo;
